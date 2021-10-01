acc50092ec0c73bfd8d963904478dbbe596a996e
Nuno Antunes <banept@gmail.com>
2021-10-01 17:16:09 +0100

    file-sniffer done

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,31 @@
 defmodule FileSniffer do
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    case extension do
+      "exe" -> "application/octet-stream"
+      "bmp" -> "image/bmp"
+      "png" -> "image/png"
+      "jpg" -> "image/jpg"
+      "gif" -> "image/gif"
+    end
   end
 
   def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+    case file_binary do
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> type_from_extension("exe")
+      <<0x42, 0x4D, _::binary>> -> type_from_extension("bmp")
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> type_from_extension("png")
+      <<0xFF, 0xD8, 0xFF, _::binary>> -> type_from_extension("jpg")
+      <<0x47, 0x49, 0x46, _::binary>> -> type_from_extension("gif")
+    end
   end
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    binary_type = type_from_binary(file_binary)
+    extension_type = type_from_extension(extension)
+
+    case binary_type do
+      ^extension_type -> {:ok, extension_type}
+      _ -> {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
