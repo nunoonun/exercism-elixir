c4f25902fc38d2a7828bb01ed08732e8e9559098
Nuno Antunes <banept@gmail.com>
2021-10-04 22:57:54 +0100

    solved it

--- boutique-suggestions/lib/boutique_suggestions.ex
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,11 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  def get_combinations(tops, bottoms, options \\ []) do
+    max_price = Keyword.get(options, :maximum_price, 100)
+    for t <- tops,
+        b <- bottoms,
+        t.base_color != b.base_color,
+        t.price + b.price <= max_price do
+      {t, b}
+    end
   end
 end
