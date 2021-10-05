2fc254835187e81bd5261e8c63270d708c6abec3
Nuno Antunes <banept@gmail.com>
2021-10-05 22:38:09 +0100

    solved it

--- community-garden/lib/community_garden.ex
+++ community-garden/lib/community_garden.ex
@@ -5,23 +5,46 @@ defmodule Plot do
 end
 
 defmodule CommunityGarden do
-  def start(opts) do
-    # Please implement the start/1 function
+  def start() do
+    Agent.start(fn -> {0, []} end)
   end
 
   def list_registrations(pid) do
-    # Please implement the list_registrations/1 function
+    Agent.get(pid, fn {_, registrations} -> registrations end)
   end
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    last_id = Agent.get(pid, fn {last_id, _} -> last_id end)
+    plot = %Plot{plot_id: last_id + 1, registered_to: register_to}
+    Agent.update(pid, fn {_, registrations} -> {last_id + 1, registrations ++ [plot]} end)
+
+    plot
   end
 
   def release(pid, plot_id) do
-    # Please implement the release/2 function
+    registration =
+      list_registrations(pid)
+      |> Enum.find(&(&1.plot_id == plot_id))
+
+    remove_registration(pid, registration)
+  end
+
+  defp remove_registration(_, nil), do: nil
+  defp remove_registration(pid, registration) do
+    Agent.update(pid, fn {id, registrations} ->
+      {id, Enum.filter(registrations, &(&1.plot_id != registration.plot_id))}
+    end)
+    :ok
   end
 
   def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
+    Agent.get(pid, fn {_, registrations} ->
+      render_registration(Enum.find(registrations, &(&1.plot_id == plot_id)))
+    end)
+  end
+
+  defp render_registration(nil), do: {:not_found, "plot is unregistered"}
+  defp render_registration(registration) do
+    registration
   end
 end
