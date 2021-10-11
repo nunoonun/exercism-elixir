1ef2b35376add0e5381406efc9c27be1d686f364
Nuno Antunes <banept@gmail.com>
2021-10-11 17:08:36 +0100

    solved with some errors

--- captains-log/lib/captains_log.ex
+++ captains-log/lib/captains_log.ex
@@ -2,18 +2,22 @@ defmodule CaptainsLog do
   @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 
   def random_planet_class() do
-    # Please implement the random_planet_class/0 function
+    Enum.random(@planetary_classes)
   end
 
   def random_ship_registry_number() do
-    # Please implement the random_ship_registry_number/0 function
+    number = :rand.uniform(8999) + 1000
+    "NCC-#{number}"
   end
 
   def random_stardate() do
-    # Please implement the random_stardate/0 function
+    new_stardate = :rand.uniform_real() * 1000.0
+    new_stardate + 41000.0
   end
 
-  def format_stardate(stardate) do
-    # Please implement the format_stardate/1 function
+  def format_stardate(stardate) when is_float(stardate) do
+    :io_lib.format("~.1f", [stardate])
+    |> List.to_string()
   end
+  def format_stardate(_), do: raise ArgumentError, message: "Stardate needs to be a float"
 end
