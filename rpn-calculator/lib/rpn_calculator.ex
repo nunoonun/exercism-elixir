918ee9cd581cbc8c3de8615c7b4a0099accde576
Nuno Antunes <banept@gmail.com>
2021-10-11 17:54:06 +0100

    solved exercise

--- rpn-calculator/lib/rpn_calculator.ex
+++ rpn-calculator/lib/rpn_calculator.ex
@@ -1,13 +1,21 @@
 defmodule RPNCalculator do
   def calculate!(stack, operation) do
-    # Please implement the calculate!/2 function
+    operation.(stack)
   end
 
   def calculate(stack, operation) do
-    # Please implement the calculate/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      _ -> :error
+    end
   end
 
   def calculate_verbose(stack, operation) do
-    # Please implement the calculate_verbose/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      e in [ArgumentError, RuntimeError] -> {:error, e.message}
+    end
   end
 end
