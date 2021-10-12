56c965f8a4172668d2d6427b6eb9b1af6f06fcfa
Nuno Antunes <banept@gmail.com>
2021-10-12 17:02:11 +0100

    solved

--- lucas-numbers/lib/lucas_numbers.ex
+++ lucas-numbers/lib/lucas_numbers.ex
@@ -5,7 +5,20 @@ defmodule LucasNumbers do
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
+  def generate(term) when not is_number(term), do: raise ArgumentError, message: "count must be specified as an integer >= 1"
+  def generate(term) when term < 1, do: raise ArgumentError, message: "count must be specified as an integer >= 1"
+  def generate(1), do: [2]
+  def generate(2), do: [2, 1]
   def generate(count) do
-    # Please implement the generate/1 function
+    3..count
+    |> Stream.scan(generate(2), &calculate/2)
+    |> Enum.take(count - 2)
+    |> List.last()
+  end
+
+  defp calculate(_, acc) do
+    {first, rem} = List.pop_at(acc, length(acc) - 1)
+    {second, _} = List.pop_at(rem, length(rem) - 1)
+    acc ++ [first + second]
   end
 end
