05d94ef30f0defbc15b64ebc9f454a302e4e6c1a
Nuno Antunes <banept@gmail.com>
2021-10-12 15:56:06 +0100

    solved exercise

--- stack-underflow/lib/rpn_calculator/exception.ex
+++ stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,5 +1,27 @@
 defmodule RPNCalculator.Exception do
-  # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception message: "division by zero occurred"
+  end
 
-  # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    defexception message: "stack underflow occurred"
+
+    def exception(term) do
+      case term do
+        [] -> %StackUnderflowError{}
+        _ -> %StackUnderflowError{message: "stack underflow occurred, context: " <> term}
+      end
+    end
+  end
+
+  def divide(stack) when length(stack) < 2, do: raise StackUnderflowError, "when dividing"
+  def divide([0 | _]), do: raise DivisionByZeroError
+  def divide(stack), do: divide_rec(stack)
+
+  def divide_rec([result]), do: result
+  def divide_rec(stack) do
+    {first, rem} = List.pop_at(stack, length(stack) - 1)
+    {second, rem} = List.pop_at(rem, length(rem) - 1)
+    divide_rec(rem ++ [first / second])
+  end
 end
